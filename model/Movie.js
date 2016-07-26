148043314d88a63113e3d6a237d7ebaa7e9e3a1c
Gin <vienlyt@gmail.com>
2016-07-26 13:30:18 -0700

    minor error handling

--- model/Movie.js
+++ model/Movie.js
@@ -5,7 +5,7 @@ const AppError = require('../lib/AppError');
 
 module.exports = function Movie(name, rating) {
   this.id = uuid.v1();
-  if (!name && !rating)
+  if (!name || !rating)
     throw AppError.error500('attempted to construct new Movie with missing properties');
   this.name = name;
   this.rating = rating;
