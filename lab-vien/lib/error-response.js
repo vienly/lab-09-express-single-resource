986bb0b72a0e1f3956279db92895cf2a4029d2f5
Gin <vienlyt@gmail.com>
2016-07-27 13:46:28 -0700

    minor change

--- lab-vien/lib/error-response.js
+++ lab-vien/lib/error-response.js
@@ -3,14 +3,13 @@
 const AppError = require('./AppError');
 
 module.exports = function() {
-  return function(req, res, next) {
-    res.sendError = function(err) {
-      if (err)
+  return (req, res, next) => {
+    res.sendError = (err) => {
+      if (err) {
         console.error(err.message);
-
-      if (AppError.isAppError(err))
-        return res.status(err.statusCode).send(err.responseMessage);
-
+        if (AppError.isAppError(err))
+          return res.status(err.statusCode).send(err.responseMessage);
+      }
       res.status(500).send('internal server problem');
     };
     next();
